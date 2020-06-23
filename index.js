425b9b90ff8bec887b6ebb56bad6645f95e54589
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-06-22 23:28:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+
+function map(inputArray, inputFunction) {
+	let output = inputArray.map(inputFunction);
+	return output
+}
+
+function reduce(inputArray, inputFunction, startingPoint) {
+	if(!startingPoint) {
+		let output = inputArray.reduce(inputFunction);
+		return output
+	} else if(startingPoint) {
+		let output = inputArray.reduce(inputFunction, startingPoint)
+		return output
+	}
+}
